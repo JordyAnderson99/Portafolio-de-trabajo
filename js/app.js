ba3578cefea1de3093f9b4e5cb14754b91b1fb33
JordyAnderson99 <60048045+JordyAnderson99@users.noreply.github.com>
2023-12-24 13:40:31 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const imagenes = document.querySelectorAll('.galeria .contenedor-imagen');
+const imagenModal = document.getElementById('imagen-modal');
+imagenes.forEach((imagen) =>{
+    imagen.addEventListener('click', ()=>{
+        const ruta = imagen.querySelector('img').src;
+        imagenModal.src = ruta;
+    })
+})
